849e55d1bac5e2e57891e4a17f084fbf7c9895a2
SergioGonzal <34321145+SergioGonzal@users.noreply.github.com>
2021-01-02 03:10:51 -0600

    A.I. challenging you

--- Proyecto2/src/app/app.module.ts
+++ Proyecto2/src/app/app.module.ts
@@ -5,14 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    HttpClientModule,
+
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
